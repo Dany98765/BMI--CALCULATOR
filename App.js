9a726b6a47f4e3324e8da8f75fed54f6a84addd7
Dany98765 <124827967+Dany98765@users.noreply.github.com>
2023-02-09 10:50:17 +0200

    App.js

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from "react"
+
+export default function App() {    
+    const [BMI, setBMICalculation] = React.useState()    
+    const [inputs, setInputs] = React.useState(
+        {
+            weight: Number(),
+            height: Number(),
+        }
+    )
+
+    function handleChange(event) {
+        setInputs(prevInputs => {
+            return {
+                ...prevInputs,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    function CalculateBMI(event){
+        event.preventDefault()
+         setBMICalculation(prevBMI => {
+            
+            return(ts.weight / ( (inputs.height) * inputs.height })  
+        })
+    
+    }
+   
+    return (
+        <div>
+            <h1 className="title" align="center">BMI Calculator</h1>
+            <form>
+                <input 
+                className="input1"
+                type="text"
+                value={inputs.weight}
+                name="weight"
+                placeholder="Type your weight in kg"
+                onChange={handleChange}
+                
+                />                
+                <input 
+                className="input2"
+                type="text"
+                value={inputs.height}
+                name="height"
+                placeholder="Type your height in metres"
+                onChange={handleChange}
+                />
+                <br />
+                <button className="button" align="center" onClick={CalculateBMI} >Calculate BMI</button>
+                <h1 align="center">BMI: <p className="BMI">{BMI}</p></h1>
+                <h4>Your BMI indicates you're weight is <h4  className="state">{BMI <= 18.5 && "UnderWeight" || BMI <= 24.9 && "Normal" || BMI <= 29.9 && "Overweight" || BMI <= 34.9 && "Obese" || BMI >= 34.9 && "Extremely Obese"}</h4></h4>
+            </form>
+        </div>
+    )
+}
